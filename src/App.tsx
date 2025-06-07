809cc37c0ddd8fa93b5ed6d0565d7598b41d9391
Arymus Reyes <arymusr@gmail.com>
2025-06-07 11:44:23 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1 className="">GitHub Repository Fetch</h1>
+        <p>Input a user's name, and get all their GitHub repositories!</p>
+      </header>
+
+      <main>
+        <input type="text" placeholder="GitHub username..."></input>
+      </main>
+
+      <footer>
+        <a href="https://github.com/arymus/repo-fetch">Source Code</a>
+        <a href="https://github.com/arymus">GitHub Page</a>
+      </footer>
+    </>
+  )
+}
+
+export default App
